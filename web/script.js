8e20415cfd93e4f9408840b26f3dbf90da7ce09c
yondemon <pablo.rod@amorodio.es>
2015-11-30 16:38:38 +0100

    Refactor

---
+++ web/script.js
@@ -0,0 +1,92 @@
+$.getScript("https://trello.com/1/client.js?key="+trellokey, function(){
+  console.log("Trello Client Script loaded.");
+
+  authorizeTrello();
+
+  loadCards("LOAD");  
+});
+
+$( "#reloadCards" ).click(function() {
+  loadCards("RELOAD");
+});
+
+
+var authorizeTrello = function(){
+  Trello.authorize({
+    type: "redirect",
+    name: "Trello2Day",
+    scope: {
+      read: true,
+      write: false },
+    expiration: "never",
+    authenticationSuccess,
+    authenticationFailure
+  });
+};
+
+
+var authenticationSuccess = function() { console.log("Successful authentication"); };
+var authenticationFailure = function() { console.log("Failed authentication"); };
+
+var loadCards = function(strMsg){
+  Trello.get('/members/me/cards/open?fields=name,due', 
+    function(data) { 
+      $( "#msg" ).html(strMsg+" OK");
+      $("#list").html("");
+
+      var todoTasks = [];
+      $.each(data,function(id,item){
+
+        if(item.due !== null){
+          todoTasks.push(item);
+        }
+      });
+
+      todoTasks.sort(function(a,b){
+        var dateA = new Date(a.due);
+        var dateB = new Date(b.due);
+        return a.due<b.due ? -1 : a.due>b.due ? 1 : 0;
+        });
+
+      $( "#msg" ).append(": "+todoTasks.length+" tasks");
+      var today = new Date();
+      var futureDay = new Date(new Date().setDate(new Date().getDate()+noFutureDays))
+      
+      $.each(todoTasks,function(id,item){
+        var itemDueDate = new Date(item.due);
+        var itemClass = "";
+
+        if(itemDueDate.getTime() > futureDay.getTime()){
+          itemClass =  itemClass + "futuretask";
+        }
+        if(itemDueDate.getTime() === today.getTime() || itemDueDate.getTime() < today.getTime()){
+          itemClass =  itemClass + "todaytask";
+        }
+        
+        var itemStr = "<li class='"+itemClass+"'><h2><a href='http://trello.com/c/"+item.id+"' target='_blank'>"+item.name+"</a></h2>"+
+          " <span class='date'>"+itemDueDate.getFullYear()+"-"+(itemDueDate.getMonth()+1)+"-"+itemDueDate.getDate()+" </span></li>"
+
+
+        $("#list").append(itemStr);
+        });
+
+
+      console.log(data);
+      },
+    function(msg){ 
+      console.log("ERROR getting");
+      $("#msg").html("Error " + msg);
+      }
+    );
+  };
+
+
+var printCards = function(data) { 
+    $( "#msg" ).html("OK");
+    data.each(function(item){
+      $("$list").append(""+item);
+    })
+    console.log(data);
+    };
+
+
